6510310126e821ecc8766b56ae3c77982519df50
Stella-Remember <139786218+Stella-Remember@users.noreply.github.com>
2025-11-03 16:21:44 +0200

    ESLint errors fixed

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -19,5 +19,12 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      // rules configuration
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react-refresh/only-export-components': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
   },
 ])
